def494842d40121c65f4b5563f19880d378c2391
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-03-10 19:44:59 +0500

    Fixing a style bug

--- pages/auth/forget-password.jsx
+++ pages/auth/forget-password.jsx
@@ -51,7 +51,7 @@ export default function ForgetPassword() {
     <>
       <NavBar />
       <section className="bg-indigo-600 dark:bg-gray-900 text-center">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
           <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
             <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Change Password
