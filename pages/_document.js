9ac746d0dde143c370f532990d7ef1759825ddd6
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-03-15 19:39:44 +0500

    creating job Details pages

--- pages/_document.js
+++ pages/_document.js
@@ -3,19 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Find Your Dream Job | Job Portal Name</title>
-        <meta name="description" content="Search and apply for the latest jobs in your field. Explore opportunities from top companies and make your career dreams come true." />
-        <meta name="keywords" content="job portal, job search, career opportunities, employment, job listings, job openings, job vacancies, job postings, hiring, recruitment" />
-        <meta name="robots" content="index, follow" />
-        <link rel="canonical" href="https://job-portal-teal.vercel.app/" />
-        <meta name="author" content="Abdullah Moiz" />
-        <meta property="og:title" content="Find Your Dream Job | Job Portal Name" />
-        <meta name="twitter:title" content="Find Your Dream Job | Job Portal Name" />
-        <meta name="language" content="en-US" />
-      </Head>
-
+      <Head/>
       <body>
         <Main />
         <NextScript />
--- pages/frontend/jobDetails/[id].jsx
+++ pages/frontend/jobDetails/[id].jsx
@@ -1,6 +1,10 @@
 import NavBar from '@/components/NavBar'
 import Image from 'next/image'
 import React from 'react'
+import { GoLocation } from 'react-icons/go'
+import { MdCategory } from 'react-icons/md'
+import { BsBriefcaseFill } from 'react-icons/bs'
+import { AiOutlineDollarCircle } from 'react-icons/ai'
 
 export default function JobDetails() {
     return (
@@ -11,11 +15,40 @@ export default function JobDetails() {
                     <h1 className='text-3xl'>Job Details</h1>
                 </div>
                 <div className='flex items-center justify-center w-full py-10'>
-                    <div className='flex w-full px-20 flex-col items-start justify-center'>
-                        <Image src={"/intro.png"} alt="no-image" width={100} height={100}/>
-                        <div className='px-4 mx-2 flex flex-col items-center justify-center'>
-                            <p>Senior Software Engineer </p>
-                            <p>Company Name</p>
+                    <div className='flex w-full px-20 items-center justify-between'>
+                        <div className='flex items-center justify-center'>
+                            <Image src={"/intro.png"} alt="no-image" width={100} height={100} />
+                            <div className='px-4 mx-2 flex flex-col items-start justify-center'>
+                                <p className='font-semibold text-base mb-1' >Senior Software Engineer </p>
+                                <p className='font-semibold text-sm text-gray-800 mb-1'>Company Name</p>
+                            </div>
+                        </div>
+                        <div className='px-4 mx-2 flex flex-col items-start justify-center'>
+                            <div className='flex items-center justify-center mb-1'>
+                                <GoLocation className='text-xs font-semibold text-indigo-600' />
+                                <p className='font-semibold text-base mx-1'>Location </p>
+                                <p className='font-semibold text-sm text-gray-800 mx-1'>Rawalipindi</p>
+                            </div>
+                            <div className='flex items-center justify-center mb-1'>
+                                <MdCategory className='text-xs font-semibold text-indigo-600' />
+                                <p className='font-semibold text-base mx-1'>Category </p>
+                                <p className='font-semibold text-sm text-gray-800 mx-1'>Graphic</p>
+                            </div>
+                        </div>
+                        <div className='px-4 mx-2 flex flex-col items-start justify-center'>
+                            <div className='flex items-center justify-center mb-1'>
+                                <BsBriefcaseFill className='text-xs font-semibold text-indigo-600' />
+                                <p className='font-semibold text-base mx-1'>Job Type </p>
+                                <p className='font-semibold text-sm text-gray-800 mx-1'>full time</p>
+                            </div>
+                            <div className='flex items-center justify-center mb-1'>
+                                <AiOutlineDollarCircle className='text-xs font-semibold text-indigo-600' />
+                                <p className='font-semibold text-base mx-1'>Salary </p>
+                                <p className='font-semibold text-sm text-gray-800 mx-1'>40000 /month</p>
+                            </div>
+                        </div>
+                        <div className='flex items-center justify-center'>
+                            <button className='px-6 py-3 bg-indigo-500 rounded text-base tracking-widest uppercase transition-all duration-700 hover:bg-indigo-900 text-white  '>Apply Position</button>
                         </div>
                     </div>
                 </div>
