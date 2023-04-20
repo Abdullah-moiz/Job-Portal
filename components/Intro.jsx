3e5091be72d11aeb3356a498c517436d2d1798b0
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-04-20 18:31:46 +0500

    adding home page filter to filter job base on categoy

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -29,7 +29,7 @@ export default function Intro() {
           <p className='md:text-lg sm:text-sm text-xs mb-20 text-gray-400'>2400 Peoples are daily search in this portal, 100 user added job portal!</p>
           <div className='bg-white flex-col mb-6 w-full md:px-4   py-4 flex sm:flex-row items-center justify-center'>
             <BiSearchAlt className='text-2xl text-indigo-600 mx-2 hidden sm:flex' />
-            <input onChange={(e) => setSearch(e.target.value)} type="text" placeholder='Search Jobs...' className='xs:w-full w-3/4  h-full px-2 bg-gray-200 text-base py-3 outline-none' />
+            <input onChange={(e) => setSearch(e.target.value)} type="text" placeholder='Search Jobs with Job categories like marketing ...' className='xs:w-full w-3/4  h-full px-2 bg-gray-200 text-base py-3 outline-none' />
             <button onClick={handleSearch} className='px-3 py-2 my-2 sm:my-0 border border-indigo-600 rounded uppercase tracking-widest mx-4   text-white bg-indigo-600 transition-all duration-700 hover:bg-transparent font-semibold text-base hover:text-indigo-600'>Search</button>
           </div>
           <div className=' w-full px-2 py-2 flex items-center justify-start flex-wrap'>
