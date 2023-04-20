382e91826c07cf98ee840c0a00e7b5160ec8af6e
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-04-20 18:31:38 +0500

    adding home page filter to filter job base on categoy

--- pages/frontend/displayJobs.jsx
+++ pages/frontend/displayJobs.jsx
@@ -1,63 +1,36 @@
 import NavBar from '@/components/NavBar'
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useRouter } from 'next/router'
-import { get_job } from '@/Services/job'
-import { setJobData } from '@/Utils/JobSlice'
-import { InfinitySpin } from 'react-loader-spinner'
+import React from 'react'
+import {  useSelector } from 'react-redux'
+
 import JobsCard from '@/components/JobsCard'
-import { toast } from 'react-toastify'
-import useSWR from 'swr'
+
 
 
 
 
 export default function DisplayJobs() {
-    const dispatch = useDispatch();
-    const router = useRouter();
-
-
-    const { data, error, isLoading } = useSWR('/getAllJobs', get_job)
-    
-    useEffect(() => {
-        if(data) dispatch(setJobData(data?.data))
-    }, [data , dispatch])
-
-
-
     const JobData = useSelector(state => state?.Job?.JobData) || [];
-    
-    
     return (
 
         <>
-            {
-                isLoading ? (
-                    <div className='bg-gray w-full h-screen flex items-center flex-col justify-center'>
-                        <InfinitySpin width='200' color="#4f46e5" />
-                        <p className='text-xs uppercase'>Loading Resources Hold Tight...</p>
-                    </div>
-                ) : (
-                    <>
-                        <NavBar />
-                        <div className='w-full  py-20 flex items-center md:px-8 px-2  justify-center flex-col'>
-                            <h1 className='px-4 mx-2 py-2 uppercase tracking-wider border-b-2 border-b-indigo-600 text-3xl font-semibold'>Available Jobs</h1>
-                            <div className='w-full h-full py-4 flex  overflow-y-auto  items-center justify-center flex-wrap'>
-                                {/* map */}
-                                {
-                                    Array.isArray(JobData) && JobData.length > 0 ? JobData?.map((job) => {
-                                        return (
-                                           <JobsCard job={job} key={job?._id} />
-                                        )
-                                    }) : <p>No jobs found</p>
-                                }
 
-                                {/* map */}
-                            </div>
-                        </div>
-                    </>
-                )
-            }
+            <NavBar />
+            <div className='w-full  py-20 flex items-center md:px-8 px-2  justify-center flex-col'>
+                <h1 className='px-4 mx-2 py-2 uppercase tracking-wider border-b-2 border-b-indigo-600 text-3xl font-semibold'>Available Jobs</h1>
+                <div className='w-full h-full py-4 flex  overflow-y-auto  items-center justify-center flex-wrap'>
+                    {/* map */}
+                    {
+                        Array.isArray(JobData) && JobData.length > 0 ? JobData?.map((job) => {
+                            return (
+                                <JobsCard job={job} key={job?._id} />
+                            )
+                        }) : <p>No jobs found</p>
+                    }
+
+                    {/* map */}
+                </div>
+            </div>
         </>
+
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,14 @@ import Intro from '@/components/Intro'
 import NavBar from '@/components/NavBar'
 import Head from 'next/head'
 import { useDispatch } from 'react-redux'
-import { setUserToken  , setUserData} from '@/Utils/UserSlice'
+import { setUserToken, setUserData } from '@/Utils/UserSlice'
 import { useEffect } from 'react'
 import Cookies from 'js-cookie'
+import { toast } from 'react-toastify'
+import useSWR from 'swr'
+import { get_job } from '@/Services/job'
+import { setJobData } from '@/Utils/JobSlice'
+import { InfinitySpin } from 'react-loader-spinner'
 
 
 
@@ -12,12 +17,19 @@ export default function Home() {
   const dispatch = useDispatch();
   const token = Cookies.get('token');
 
+
+  const { data, error, isLoading } = useSWR('/getAllJobs', get_job)
+
+  useEffect(() => {
+    if (data) dispatch(setJobData(data?.data))
+  }, [data, dispatch])
+
+
   useEffect(() => {
     if (token) {
       dispatch(setUserToken(token))
     }
-    else
-    {
+    else {
       localStorage.removeItem('user')
       dispatch(setUserData(null))
     }
@@ -41,10 +53,21 @@ export default function Home() {
         <meta name="language" content="en-US" />
       </Head>
 
-      <NavBar />
-      <div className="w-full h-screen bg-gray-200  text-black">
-        <Intro />
-      </div>
+      {
+        isLoading ? (
+          <div className='bg-gray w-full h-screen flex items-center flex-col justify-center'>
+            <InfinitySpin width='200' color="#4f46e5" />
+            <p className='text-xs uppercase'>Loading Resources Hold Tight...</p>
+          </div>
+        ) : (
+          <>
+            <NavBar />
+            <div className="w-full h-screen bg-gray-200  text-black">
+              <Intro />
+            </div>
+          </>
+        )
+      }
     </>
   )
 }
